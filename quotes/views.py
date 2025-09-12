f916d419a6c656b642a994d005ae00aec70c7876
Amy <aho@bu.edu>
2025-09-12 17:47:18 -0400

    added files for deployment and updated image handling in show_all.html

--- quotes/views.py
+++ quotes/views.py
@@ -1,12 +1,22 @@
 # file: quotes/views.py
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
 
 import random
 
 # two lists. one of quotes, another of images
-list_quotes= ["The man who says he can, and the man who says he can't are both correct.", "A great man is hard on himself; a small man is hard on others", "The gem can not be polished without friction, nor man perfected without trials."]
-list_images= ["https://i.natgeofe.com/n/d7b042d8-ee0b-48a2-935c-5eddbbec0ed7/02-confucius.jpg", "https://images.chinahighlights.com/allpicture/2014/09/1641a5d991e84d21a23aa5ca_cut_600x550_241_1742308020.jpg", "https://taniamacedoreligionisu.weebly.com/uploads/7/8/2/2/78226348/318606103.jpg?306"]
+list_quotes= [
+              "The man who says he can, and the man who says he can't are both correct.", 
+              "A great man is hard on himself; a small man is hard on others.", 
+              "The gem can not be polished without friction, nor man perfected without trials.",
+              "It does not matter how slowly you go as long as you do not stop.",
+              "We should feel sorrow, but not sink under its oppression.",
+              ]
+list_images= ["https://i.natgeofe.com/n/d7b042d8-ee0b-48a2-935c-5eddbbec0ed7/02-confucius.jpg",
+               "https://images.chinahighlights.com/allpicture/2014/09/1641a5d991e84d21a23aa5ca_cut_600x550_241_1742308020.jpg",
+                "https://taniamacedoreligionisu.weebly.com/uploads/7/8/2/2/78226348/318606103.jpg?306",
+                "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/confucius-chinese-school.jpg",
+                "https://upload.wikimedia.org/wikipedia/commons/5/54/Confucius_Tang_Dynasty.jpg"
+                ]
 
 # Create your views here.
 def home(request):
