ffc4492fae3ee65a353de7c585092654a622f61c
Amy <aho@bu.edu>
2025-10-26 21:00:56 -0400

    fixed error where clicking on a profile without being logged in redirects to login page.

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -20,7 +20,7 @@ class ProfileListView(ListView):
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
 
-class ProfileView(ProfileLoginRequiredMixin, DetailView):
+class ProfileView(DetailView):
     '''Display a single profile.'''
 
     model = Profile
